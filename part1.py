c8a44dfbc12f646b39a25cead2590d087bfe7bd1
Mandrach <daniel_amg@web.de>
2020-01-14 17:00:21 +0100

    Finished Part 1 completely (Nr. 2 finished)

--- part1.py
+++ part1.py
@@ -37,19 +37,23 @@ with open ('precipitation.json') as json_file:
         
 # 2. Sum all measurements and save them in a list
     # We have to go through the data, check for the station, and then sort by date. 
-    # 1. does already go through all the data. But how to sort by date?
-    # how can I split the date by month? 
-    measurement_of_station = [
+
+
+    measurement_of_station = [ # creating a list of twelf zeros, one vessel for each month
         0
     ]*12
+    # going through the data set and calculating the sum of precipitation for each month
     for measurement in precipitation_data:
         date_parsed = measurement['date'].split('-')
-        if measurement['station'] == station_code:
-            if date_parsed[2] == '01':
-                    measurement_of_station += measurement['value'] # creating the sum for current month
+        if measurement['station'] == station_code:              # checks for the correct station
+            for month in range(len(measurement_of_station)):    # 12 months, 12 sums...
+                if int(date_parsed[1]) == month+1:              # string difficult, int easy
+                    measurement_of_station[month] += measurement['value'] # creating the sum for current month
 
 # save the result as a json file
-
+#with  open(’myJSONfile.json’, ’w’) as f:
+    # open  JSON  file3json.dump(my_dictionary 
+ #   , f, indent=4, sort_keys=True)
 
 # Part 2
     # Nr. 1 - Percipitation value for the whole year
@@ -64,7 +68,7 @@ print(f'The percipitation for the year is: {percipitation_year}')
 relative_per_month = {} 
 for month in range(len(measurement_of_station)):
     relative_per_month[month] = measurement_of_station[month] / percipitation_year
-print(f'The relative percipitation for each month for the station Seattle is: {relative_per_month}')
+print(f'The relative percipitation for each month for the station Seattle is: {relative_per_month}. Legend: 0 = January, 1 = February, etc.')
     
 
 
