7310120669d0f6496936a8c606431e34316d2ee9
Mandrach <daniel_amg@web.de>
2020-01-14 17:48:10 +0100

    Added Part 3 completely
    But, full json file still missing, sadly

--- part1.py
+++ part1.py
@@ -5,18 +5,20 @@ import json
 #3. Automating everything
 # 
 with open ('stations.csv') as csv_file:
-    csv_file = csv_file.read()
-    csv_file = file_contents.split(',')
-
+    csv_file.readline() # read  first  line  and  move  pointer to  second  line
+    csv_file = csv_file.readlines()
+    #csv_file = csv_file.split(',')
+    
     file_dict = {}         # initialize  dictionary
-    headers = csv_file.readline() # read  first  line  and  move  pointer to  second  line
-    for  line in csv_file:        # for  2nd  line  onward
+    
+    for  line in csv_file: # for  2nd  line  onward
+        #print(line)  
         location, state, station = line.strip().split(',') #remove white space  and  then  split  by  comma
         file_dict[location] = {
             'station': station,
             'state': state
         }
-for file_dict[location] in file_dict
+for location in file_dict:
            # print(file_dict[location])
 
     # 1. load in json data & find 
@@ -24,33 +26,14 @@ for file_dict[location] in file_dict
         precipitation_data = json.load(json_file)
         # Use this station code to select all the measurements belonging to it from the JSON data
         #precipitation_dict = {}
-        station_code = file_dict[station]
-            for measurement in precipitation_data:
-                if measurement['station'] == station_code:
-                    print(measurement['value'])
-    
-    # 2. Sum all measurements and save them in a list
-    # We have to go through the data, check for the station, and then sort by date.  
-        measurement_of_station = []
-        measurement['date'].split('-')
-            for measurement in precipitation_data:
-                if measurement['station'] == station_code:
-                    for month in measurement['date']:
-                        while measurement['date'][2] == month:
-                            measurement_of_station[measurement] += measurement['value']
-        print(measurement_of_station)
-
-    	# save the result as a json file
-
-
-    for measurement in precipitation_data:
-        if measurement['station'] == station_code:
-            pass
-            #print(measurement['value'])
+        station_code = file_dict[location]['station']
+        for measurement in precipitation_data:
+            if measurement['station'] == station_code:
+                #print(measurement['value'])
+                pass
         
 # 2. Sum all measurements and save them in a list
     # We have to go through the data, check for the station, and then sort by date. 
-
 
     measurement_of_station = [ # creating a list of twelf zeros, one vessel for each month
         0
@@ -63,24 +46,25 @@ for file_dict[location] in file_dict
                 if int(date_parsed[1]) == month+1:              # string difficult, int easy
                     measurement_of_station[month] += measurement['value'] # creating the sum for current month
 
-# save the result as a json file
-with  open('myJSONfile.json', 'w') as f:                    # open  JSON  file
-    json.dump(measurement_of_station, f, indent=4, sort_keys=True)   # write  my_dictionary  to  file  in a  pretty  JSON  format
+    # save the result as a json file
+    with  open('myJSONfile.json', 'w') as f:                    # open  JSON  file
+        json.dump(measurement_of_station, f, indent=4, sort_keys=True)   # write  my_dictionary  to  file  in a  pretty  JSON  format
    
 
 # Part 2
     # Nr. 1 - Percipitation value for the whole year
-percipitation_year = sum(measurement_of_station)
+    percipitation_year = sum(measurement_of_station)
+    print(f'The percipitation for the whole year for station {location} is {percipitation_year}')
 
     # Nr. 2 - Calculate the relative precipitation per month 
     # (percentage compared to the precipitation over the whole year)
 
-# a dictionary which should contain 12 entries, representing the relative value for each month
-# month is the key, the relative value of the calculation is the value
-relative_per_month = {} 
-for month in range(len(measurement_of_station)):
-    relative_per_month[month] = measurement_of_station[month] / percipitation_year
-print(f'The relative percipitation for each month for the station Seattle is: {relative_per_month}. Legend: 0 = January, 1 = February, etc.')
+    # a dictionary which should contain 12 entries, representing the relative value for each month
+    # month is the key, the relative value of the calculation is the value
+    relative_per_month = {} 
+    for month in range(len(measurement_of_station)):
+        relative_per_month[month] = measurement_of_station[month] / percipitation_year
+    print(f'The relative percipitation for each month for the station Seattle is: {relative_per_month}. Legend: 0 = January, 1 = February, etc.')
     
 
 
